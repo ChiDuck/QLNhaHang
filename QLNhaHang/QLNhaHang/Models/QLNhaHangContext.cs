7654f799e3df88d85d97e4e00671ff7dd9262a42
ChiDuck <cheesieduck@gmail.com>
2025-08-04 14:49:16 +0700

    new file look

--- QLNhaHang/QLNhaHang/Models/QLNhaHangContext.cs
+++ QLNhaHang/QLNhaHang/Models/QLNhaHangContext.cs
@@ -46,7 +46,7 @@ namespace QLNhaHang.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=chiduck;Initial Catalog=QLNhaHang;Integrated Security=True;Trust Server Certificate=True");
+                optionsBuilder.UseSqlServer("QLNhaHangContext");
             }
         }
 
